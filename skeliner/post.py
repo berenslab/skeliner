c0272865cf52a20e0f4975d5ef19cdacd4496efd
Jonathan Oesterle <jonathan.oesterle.work@gmail.com>
2025-11-20 10:51:01 +0100

    Refine calibration.

--- skeliner/post.py
+++ skeliner/post.py
@@ -6,9 +6,7 @@ from typing import Iterable, Set, cast
 
 import igraph as ig
 import numpy as np
-import trimesh
 from numpy.typing import ArrayLike
-from tqdm import tqdm
 
 from . import dx
 from ._core import (
